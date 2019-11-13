2ab00bb99f4fcf2dac57489f4e48917d65acef01
Dejiang Chen <dejiang27@gmail.com>
2019-11-12 20:30:38 -0600

    Generated components, services, and guard

--- business-cards/src/app/app-routing.module.ts
+++ business-cards/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import {LoginComponent} from './login/login.component';
+import {NotFoundComponent} from './not-found/not-found.component';
+import {BusinessCardsComponent} from './business-cards/business-cards.component';
+import {AuthGuardGuard} from './auth-guard.guard';
+const routes: Routes = [
 
-const routes: Routes = [];
+  {path: '', redirectTo:'login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+
+  {path:'business-cards', component:BusinessCardsComponent, canActivate:[AuthGuardGuard] },
+
+  {path:'**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- business-cards/src/app/app.module.ts
+++ business-cards/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { BusinessCardComponent } from './business-card/business-card.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { BusinessCardsComponent } from './business-cards/business-cards.component';
+import { LogoutComponent } from './logout/logout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    BusinessCardComponent,
+    NotFoundComponent,
+    BusinessCardsComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
