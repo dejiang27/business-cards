34be628d15d9d4c483c8ec9d6fbf684c831667cc
Dejiang Chen <dejiang27@gmail.com>
2019-11-16 18:17:30 -0600

    Update app.module.ts

--- business-cards/src/app/app.module.ts
+++ business-cards/src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { FormsModule }   from '@angular/forms';
+import { environment} from '../environments/environment';
+//Import components
 import { LoginComponent } from './login/login.component';
 import { BusinessCardComponent } from './business-card/business-card.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { BusinessCardsComponent } from './business-cards/business-cards.component';
 import { LogoutComponent } from './logout/logout.component';
+//Import services
+import {AuthServiceService} from './services/auth-service.service';
+import {BusinessCardService} from './services/business-card.service';
+import {SearchService} from './services/search.service';
+//Import Firebase
+import { firestore } from 'firebase';
+import {AngularFireModule} from '@angular/fire';
+import {AngularFirestore} from '@angular/fire/firestore';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [
@@ -21,9 +33,19 @@ import { LogoutComponent } from './logout/logout.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase, 'firebaseApp'),
+    AngularFireAuthModule,
+    AngularFireModule,
+    AngularFireStorageModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    AuthServiceService,
+    BusinessCardService,
+    SearchService,
+    AngularFirestore
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
